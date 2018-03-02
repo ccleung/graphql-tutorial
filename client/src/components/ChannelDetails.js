e9ec6d6bc4e4bb4e845aa0b9e7a6f78ad013ba64
Clement Leung <clement.leung@shopify.com>
2018-03-02 13:28:19 -0500

    complete add message part 5

--- client/src/components/ChannelDetails.js
+++ client/src/components/ChannelDetails.js
@@ -8,10 +8,20 @@ import {
     graphql,
 } from 'react-apollo';
 
-const ChannelDetails = () => {
-  let messages = [{id:'1', text:"Stub Message - To Replace"}];
-  let name = "Stub Name";
-  let channel = {name, messages};
+const ChannelDetails = ({ data: { loading, error, channel }, match }) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  if(channel === null) {
+    return <NotFound />
+  }
+
+  // let messages = [{id:'1', text:"Stub Message - To Replace"}];
+  // let name = "Stub Name";
+  // let channel = {name, messages};
 
   return (
     <div>
@@ -22,4 +32,26 @@ const ChannelDetails = () => {
     </div>);
 }
 
-export default (ChannelDetails);
+export const channelDetailsQuery = gql`
+  query ChannelDetailsQuery($channelId : ID!) {
+    channel(id: $channelId) {
+      id
+      name
+      messages {
+        id
+        text
+      }
+    }
+  }
+`;
+
+const ChannelDetailsWithQuery = graphql(
+  channelDetailsQuery,
+  { 
+    options: (props) => ({
+      variables: { channelId: props.match.params.channelId },
+    }),
+  }
+)(ChannelDetails);
+
+export default ChannelDetailsWithQuery;
